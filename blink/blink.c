d94186acfb69e22b480b290819b823c252930a1f
tatianaanthony <tatiana.anthony@students.olin.edu>
2017-01-23 00:04:55 -0500

    blink update

--- blink/blink.c
+++ blink/blink.c
@@ -14,12 +14,26 @@ int16_t main(void) {
     timer_start(&timer2);
 
     while (1) {
-        if (timer_flag(&timer2)) {
-            timer_lower(&timer2);
-            led_toggle(&led1);
+
+        else if (timer_flag(&timer2)) { 
+            timer_lower(&timer2){
+
+            }
+            if (!sw_read(&sw2)){
+        		led_write(led_write(&led3, led_read(&led2));)
+        		led_write(led_write(&led2, led_read(&led1));)
+        		led_write(led_write(&led1, led_read(&led3));)
+            }
+            else{
+            	led_toggle(&led1);
+            	if(!sw_read(&sw2)) {
+            		led_toggle(&led2)
+           	 	}
+       	 	}	
+
+            
+           
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
     }
 }
 
